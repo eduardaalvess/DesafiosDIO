e85ba96cfbac700ceff58116cd0b099f3f21cb91
saintravi <Dudah4@gmail.com>
2021-08-17 02:45:54 -0300

    desafios decola tech + cognizant cloud data engineer

--- Desafios DIO - C Sharp/distancia.cs
+++ Desafios DIO - C Sharp/distancia.cs
@@ -1,14 +1,25 @@
 /*
 
-DESAFIO: Duas motos (X e Y) partem em uma mesma direção. A moto X sai com velocidade constante de 60 Km/h e a moto Y sai com velocidade constante de 90 Km/h.
+DESAFIO - Distancia
 
-Em uma hora (60 minutos) a moto Y consegue se distanciar 30 quilômetros da moto X, ou seja, consegue se afastar um quilômetro a cada 2 minutos.
+Duas motos (X e Y) partem em uma mesma direção. 
+A moto X sai com velocidade constante de 60 Km/h e a moto Y sai com velocidade constante de 90 Km/h.
 
-O seu desafio é ler a distância (em Km) e calcular quanto tempo leva (em minutos) para a moto Y tomar essa distância da outra moto.
+Em uma hora (60 minutos) a moto Y consegue se distanciar 
+30 quilômetros da moto X, ou seja, consegue se afastar um quilômetro a cada 2 minutos.
 
-Entrada: O arquivo de entrada contém um número inteiro K que representa a quantidade de quilômetro que que a moto Y deve estar da moto X.
+O seu desafio é ler a distância (em Km) e calcular quanto tempo leva 
+(em minutos) para a moto Y tomar essa distância da outra moto.
 
-Saída: Imprima o tempo necessário para a moto Y ficar com a quantidade K de quilômetro da moto X, seguido da mensagem " minutos".
+Entrada - 
+
+O arquivo de entrada contém um número inteiro K que representa a quantidade de 
+quilômetro que que a moto Y deve estar da moto X.
+
+Saída -
+
+Imprima o tempo necessário para a moto Y ficar com a quantidade 
+K de quilômetro da moto X, seguido da mensagem " minutos".
 
 
 */
--- Desafios DIO - C Sharp/multiplos.cs
+++ Desafios DIO - C Sharp/multiplos.cs
@@ -1,4 +1,5 @@
-/*
+/* Desafio - Multiplos
+
 Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem
 "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são
 múltiplos entre si.
